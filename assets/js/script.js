f1dab698fd274527fa01d52e34e17e4ce98e357a
Ahmed Bahaa <mrxeg1995@gmail.com>
2024-10-17 03:19:02 +0300

    first

---
+++ assets/js/script.js
@@ -0,0 +1,95 @@
+'use strict';
+
+
+
+/**
+ * add event on element
+ */
+
+const addEventOnElem = function (elem, type, callback) {
+  if (elem.length > 1) {
+    for (let i = 0; i < elem.length; i++) {
+      elem[i].addEventListener(type, callback);
+    }
+  } else {
+    elem.addEventListener(type, callback);
+  }
+}
+
+
+
+/**
+ * navbar toggle
+ */
+
+const navbar = document.querySelector("[data-navbar]");
+const navToggler = document.querySelectorAll("[data-nav-toggler]");
+const overlay = document.querySelector("[data-overlay]");
+
+const toggleNavbar = function () {
+  navbar.classList.toggle("active");
+  overlay.classList.toggle("active");
+}
+
+addEventOnElem(navToggler, "click", toggleNavbar);
+
+
+
+/**
+ * close navbar when click on any navbar links
+ */
+
+const navLinks = document.querySelectorAll("[data-nav-link]");
+
+const closeNavbar = function () {
+  navbar.classList.remove("active");
+  overlay.classList.remove("active");
+}
+
+addEventOnElem(navLinks, "click", closeNavbar);
+
+
+
+/**
+ * header active when scroll down
+ */
+
+const header = document.querySelector("[data-header]");
+
+const headerActive = function () {
+  window.scrollY > 100 ? header.classList.add("active")
+    : header.classList.remove("active");
+}
+
+addEventOnElem(window, "scroll", headerActive);
+
+
+
+/**
+ * accordion toggle
+ */
+
+const accordionAction = document.querySelectorAll("[data-accordion-action]");
+
+const toggleAccordion = function () { this.classList.toggle("active"); }
+
+addEventOnElem(accordionAction, "click", toggleAccordion);
+
+
+
+/**
+ * SCROLL REVEAL
+ */
+
+ const revealElements = document.querySelectorAll("[data-reveal]");
+
+ const scrollReveal = function () {
+   for (let i = 0, len = revealElements.length; i < len; i++) {
+     if (revealElements[i].getBoundingClientRect().top < window.innerHeight / 1.2) {
+       revealElements[i].classList.add("revealed");
+     }
+   }
+ }
+ 
+ window.addEventListener("scroll", scrollReveal);
+ window.addEventListener("load", scrollReveal);
